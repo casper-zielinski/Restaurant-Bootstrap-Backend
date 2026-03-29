70308206e4f8cc8ae90c52e8f2d7bd2fc8814fa0
casper-zielinski <casper.zielinski@edu.fh-joanneum.at>
2026-03-29 21:45:28 +0200

    test: unit testing Reservation Entity de-serialization

--- src/test/java/com/tavernaluna/backend/ReservationTest.java
+++ src/test/java/com/tavernaluna/backend/ReservationTest.java
@@ -15,6 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @JsonTest
 class ReservationTest {
+    private final String fileName = "expected.json";
 
     @Autowired
     private JacksonTester<Reservation> json;
@@ -22,7 +23,20 @@ class ReservationTest {
     @Test
     void reservationSerializationTest() throws IOException {
         Reservation reservation = new Reservation("r5", LocalTime.MIN, "Casper Zielinski", LocalDate.EPOCH, null, "+43 343435 3553434 434", 12.22, List.of(true, true, false), "123-345");
-        assertThat(json.write(reservation)).isStrictlyEqualToJson("expected.json");
+        assertThat(json.write(reservation)).isStrictlyEqualToJson(fileName);
         assertThat(json.write(reservation)).hasJsonPathArrayValue("@.tables");
+        assertThat(json.write(reservation)).hasJsonPathValue("$.id", reservation.getId());
+        assertThat(json.write(reservation)).hasJsonPathValue("$.userId", reservation.getUserId());
+        assertThat(json.write(reservation)).extractingJsonPathValue("$.email").isNull();
+        assertThat(json.write(reservation)).hasJsonPathNumberValue("$.price", reservation.getPrice());
+        assertThat(json.write(reservation)).doesNotHaveJsonPath("$.IBAN");
+    }
+
+    @Test
+    void reservationDeserializationTest() throws Exception {
+        Reservation reservation = json.readObject(fileName);
+        assertThat(reservation.getEmail()).isNull();
+        assertThat(reservation.getId()).isEqualTo("r5");
+        assertThat(reservation.getTime()).isEqualTo(LocalTime.MIN);
     }
 }
